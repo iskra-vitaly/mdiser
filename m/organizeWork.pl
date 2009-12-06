5042f4ef503558c218809fcba8121bf208b5fcd8
Vitalik <vitalik@existencia.(none)>
2009-12-06 13:27:48 +0200

    script to organize generated bootstrap images

---
+++ m/organizeWork.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use File::Path qw(make_path remove_tree);
+
+
+$#ARGV >= 1 || die("No enough arguments"); 
+
+
+my($wd) = $ARGV[0];
+my($od) = $ARGV[1];
+
+print "Work dir: $wd\nDest dir: $od\n";
+
+foreach(glob("$wd/*.jpg")) {
+	if (/(\d{2})\.(\d{4})\.jpg$/) {
+		my($subj, $cond) = ($1, $2);
+		my($subjdir,$conddir) = ("$od/bysubj/$subj", "$od/bycond/$cond");
+		File::Path::make_path($subjdir, $conddir);
+		link($_, "$subjdir/$cond.jpg");
+		link($_, "$conddir/$subj.jpg");
+
+	}
+}
